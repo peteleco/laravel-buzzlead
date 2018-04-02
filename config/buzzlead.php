a81019c2dde4112845ed84ba7ed3ed977c02882e
Leonardo Jorge <ldiascarmo@gmail.com>
2018-04-02 13:33:38 -0300

    Config para verificar se a conversão por embaixador está ativa.

--- config/buzzlead.php
+++ config/buzzlead.php
@@ -1,26 +1,28 @@
 <?php
 return [
-    'env'              => env('BUZZLEAD_ENV', 'sandbox'),
-    'urls'             => [
+    'enabled'            => env('BUZZLEAD_ENABLED', true),
+    'enabled_ambassador' => env('BUZZLEAD_ENABLED_AMBASSADOR', true),
+    'env'                => env('BUZZLEAD_ENV', 'sandbox'),
+    'urls'               => [
         'sandbox'    => env('BUZZLEAD_URL_SANDBOX', 'http://test.buzzlead.com.br'),
         'production' => env('BUZZLEAD_URL_PRODUCTION', 'https://app.buzzlead.com.br'),
     ],
-    'script'           => [
+    'script'             => [
         'sandbox'    => env('BUZZLEAD_SCRIPT_SANDBOX', 'http://test.buzzlead.com.br/tracker.js'),
         'production' => env('BUZZLEAD_SCRIPT_PRODUCTION', 'https://app.buzzlead.com.br/tracker.js'),
     ],
-    'api'              => [
+    'api'                => [
         'email' => env('BUZZLEAD_API_EMAIL', ''),
         'token' => env('BUZZLEAD_API_TOKEN', ''),
         'key'   => env('BUZZLEAD_API_KEY', '')
     ],
     // Main campaign
-    'campaign'         => env('BUZZLEAD_MAIN_CAMPAIGN', ''),
-    'cookie'           => [
+    'campaign'           => env('BUZZLEAD_MAIN_CAMPAIGN', ''),
+    'cookie'             => [
         'original' => env('BUZZLEAD_COOKIE_ORIGINAL', 'buzzlead'),
         'updated'  => env('BUZZLEAD_COOKIE_UPDATED', 'buzzlead_affiliate'),
         'expiry'   => env('BUZZLEAD_COOKIE_EXPIRY', 10080),
     ],
     // Quantos dias vai levar para ser processado
-    'days_to_proccess' => env('BUZZLEAD_DAYS_TO_PROCCESS', 10),
+    'days_to_proccess'   => env('BUZZLEAD_DAYS_TO_PROCCESS', 10),
 ];
